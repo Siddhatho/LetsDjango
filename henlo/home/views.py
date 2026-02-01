7f03f68f85b05256dccea4982d7786673b797447
Siddartho Sarker Bipro <133673434+Siddhatho@users.noreply.github.com>
2026-02-01 14:24:06 +0600

    Add home app views and URL dispatching
    Add home/urls.py with routes for index, about, contact and services, and implement simple HttpResponse-based view functions in home/views.py. Wire the home app into the project by adding include('home.urls') to henlo/henlo/urls.py. Binary DB and __pycache__ files updated as part of this change.

--- henlo/home/views.py
+++ henlo/home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("This is home page")
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+
+def services(request):
+    return HttpResponse("This is services page")
