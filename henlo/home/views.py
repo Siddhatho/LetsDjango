8ea97fd1a2a95bf6063777e0dd76df7e39f990b9
Siddartho Sarker Bipro <133673434+Siddhatho@users.noreply.github.com>
2026-02-24 20:28:13 +0600

    Add Contact model, contact view and messaging
    Create Contact model and initial migration; register Contact in admin. Implement contact form handling in views (save Contact, set messages.success) and add message display markup to base template. Add home.apps.HomeConfig to INSTALLED_APPS and import message constants in settings. Database (db.sqlite3) and various compiled files updated accordingly.

--- henlo/home/views.py
+++ henlo/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -14,7 +17,15 @@ def about(request):
     #return HttpResponse("This is about page")
 
 def contact(request):
-    
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, "Message sent successfully")
+
     return render(request,'contact.html')
 
 def services(request):
